b696d79407a365c8c987284da3314bbe050eb3c8
LeeTaehee <prussian1933@naver.com>
2023-01-20 13:28:13 +0900

    Feat: Backend Entity 추가

--- development/Backend/studymoim/src/main/java/com/ssafy/peace/repository/UserRepository.java
+++ development/Backend/studymoim/src/main/java/com/ssafy/peace/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.ssafy.peace.repository;
 
+import com.ssafy.peace.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
