8ab6b8dd1e4e7c81d42f0bd7f6e0e46318ed9775
박서영 <manchott@naver.com>
2023-01-20 16:02:39 +0900

    Feat: Backend F~ 엔티티 작성중

--- development/Backend/studymoim/src/main/java/com/ssafy/peace/entity/FreeBoard.java
+++ development/Backend/studymoim/src/main/java/com/ssafy/peace/entity/FreeBoard.java
@@ -1,12 +1,38 @@
 package com.ssafy.peace.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.sql.Timestamp;
 
+@Entity
+@Table(name = "FREE_BOARD")
+@Getter
+@NoArgsConstructor
 public class FreeBoard {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "free_board_id")
     private int freeBoardId;
+
+    @Column(name = "title")
+    @Size(max = 20)
+    @NotNull
     private String title;
+
+    @Column(name = "content")
+    @NotNull
     private String content;
+
+    @Column(name = "publish_time")
+    @NotNull
     private Timestamp publishTime;
+
+    @Column(name = "is_deleted")
     private boolean isDeleted;
     private int hit;
     private int userId;
