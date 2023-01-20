77984fde2606fee41369cf85e84fa36f26e1ca4b
junho <blacktick@naver.com>
2023-01-20 14:33:16 +0900

    Feat: Backend Entity User 상세

--- development/Backend/studymoim/src/main/java/com/ssafy/peace/entity/User.java
+++ development/Backend/studymoim/src/main/java/com/ssafy/peace/entity/User.java
@@ -1,6 +1,7 @@
 package com.ssafy.peace.entity;
 
 import lombok.*;
+import org.hibernate.annotations.Columns;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -14,30 +15,50 @@ import java.sql.Timestamp;
 @AllArgsConstructor
 @Getter
 @Entity
-//@TypeD
 public class User {
+
+    // Todo 컬럼
 
     @Id
     @GeneratedValue
-    @Column(name = "user_id", length = 64, unique = true)
-    @NotNull
-    @Size(max = 64)
+    @Column(name = "user_id")
     private int userId;
 
+    @Column(name = "email")
+    @Size(max = 50)
+    @NotNull
     private String email;
 
+    @Column(name = "password")
+    @Size(max = 20)
+    @NotNull
     private String password;
 
+    @Column(name = "nickname")
+    @Size(max = 10)
+    @NotNull
     private String nickname;
 
+    @Column(name = "save_name")
+    @Size(max = 255)
     private String saveName;
 
+    @Column(name = "register_date")
+    @NotNull
     private Timestamp registerDate;
 
+    @Column(name = "last_access_time")
     private Timestamp lastAccessTime;
 
+    @Column(name = "is_quit")
+    @NotNull
     private boolean isQuit;
 
+    @Column(name = "quit_time")
     private Timestamp quitTime;
 
+
+
+
+    // Todo 연결...
 }
