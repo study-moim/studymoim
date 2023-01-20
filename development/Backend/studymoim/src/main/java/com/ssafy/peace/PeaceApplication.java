08b437410a9aa8449e6eb1e6bb2e0fd1129cd486
LeeTaehee <prussian1933@naver.com>
2023-01-20 10:11:03 +0900

    Feat: Backend 베이스 깔아놓음

---
+++ development/Backend/studymoim/src/main/java/com/ssafy/peace/PeaceApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.peace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeaceApplication.class, args);
+	}
+
+}
---
+++ development/Backend/studymoim/src/test/java/com/ssafy/peace/PeaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.peace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
