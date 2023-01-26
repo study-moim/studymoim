4d511d2cdb3efb2b92d361d889ed32c8ce0ed0ea
junho <blacktick@naver.com>
2023-01-26 17:45:30 +0900

    Feat: Backend 유튜브 api 강좌, 강의 db에 넣기 ing...

--- development/Backend/studymoim/src/main/java/com/ssafy/peace/service/YoutubeApiService.java
+++ development/Backend/studymoim/src/main/java/com/ssafy/peace/service/YoutubeApiService.java
@@ -14,11 +14,15 @@ import com.google.api.services.youtube.model.PlaylistItem;
 import com.google.api.services.youtube.model.Video;
 import com.ssafy.peace.entity.Course;
 import com.ssafy.peace.entity.Lecture;
+import com.ssafy.peace.repository.CourseRepository;
 import com.ssafy.peace.repository.LectureRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
@@ -29,14 +33,67 @@ public class YoutubeApiService {
 
     private final LectureRepository lectureRepository;
 
+    private final CourseRepository courseRepository;
+
+
     // 추후 키 관리
     private static String requestKey = "AIzaSyBhWMg2moTtilibBevJSI3M9iWUwmkRImQ";
     private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
     private static final JsonFactory JSON_FACTORY = new JacksonFactory();
     private static YouTube youtube;
 
+    /**
+     * 특정 유저가 올린 강좌 모두 DB에 등록.
+     * @param channelId
+     * @return
+     */
+    public List<Playlist> getPlayList(String channelId) {
+        try {
+            youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpRequestInitializer() {
+                public void initialize(HttpRequest request) throws IOException {
+                }
+            }).setApplicationName("youtube-playlist-get").build();
 
-    public String getPlayListItem(String playlistId) {
+            YouTube.Playlists.List playlistRequest =
+                    youtube.playlists().list("id,contentDetails,snippet");
+            playlistRequest.setKey(requestKey);
+            playlistRequest.setChannelId(channelId);
+            playlistRequest.setMaxResults(1000l);
+            List<Playlist> playLists = playlistRequest.execute().getItems();
+            if(playLists != null) {
+                int n = playLists.size();
+                for(int i = 0; i < n; i++) {
+                    // 데이터 가져오기
+//                    System.out.println(playLists.get(i).getSnippet().getChannelId());
+                    playLists.get(i).getSnippet();
+                    Course course = Course.builder()
+                            .title(playLists.get(i).getSnippet().getTitle())
+                            .content(playLists.get(i).getSnippet().getLocalized().getTitle())
+                            .build();
+
+                    courseRepository.save(course);
+                }
+            }
+            return playLists;
+
+        } catch (GoogleJsonResponseException e) {
+            System.err.println("There was a service error: " + e.getDetails().getCode() + " : "
+                    + e.getDetails().getMessage());
+        } catch (IOException e) {
+            System.err.println("There was an IO error: " + e.getCause() + " : " + e.getMessage());
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * 강좌에 있는 모든 모든 강의 DB에 기록
+     * @param playlistId
+     * @return
+     */
+    public List<PlaylistItem> getPlayListItem(String playlistId) {
         try {
             youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpRequestInitializer() {
                 public void initialize(HttpRequest request) throws IOException {
@@ -61,12 +118,16 @@ public class YoutubeApiService {
                             .content("")
                             .url("sdf")
                             .build();
+
+
+                    // Service -> Service
+                    getVideoInfo(playListItems.get(i).getSnippet().getResourceId().getVideoId(), lecture);
+
                     lectureRepository.save(lecture);
-                    System.out.println(playListItems.get(i).getSnippet().getTitle().toString());
                 }
             }
 
-            return playListItems.toString();
+            return playListItems;
 
         } catch (GoogleJsonResponseException e) {
             System.err.println("There was a service error: " + e.getDetails().getCode() + " : "
@@ -79,30 +140,24 @@ public class YoutubeApiService {
         return null;
     }
 
-    public String getPlayList(String channelId) {
+    public List<Video> getVideoInfo(String videoId, Lecture lecture) {
         try {
             youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpRequestInitializer() {
                 public void initialize(HttpRequest request) throws IOException {
                 }
-            }).setApplicationName("youtube-playlist-get").build();
+            }).setApplicationName("youtube-video-get").build();
 
-            YouTube.Playlists.List playlistRequest =
-                    youtube.playlists().list("id,contentDetails,snippet");
-            playlistRequest.setKey(requestKey);
-            playlistRequest.setChannelId(channelId);
-            playlistRequest.setMaxResults(1000l);
-            List<Playlist> playLists = playlistRequest.execute().getItems();
-            if(playLists != null) {
-                int n = playLists.size();
-                for(int i = 0; i < n; i++) {
-                    // 데이터 가져오기
-//                    System.out.println(playLists.get(i).getSnippet().getChannelId());
-                    playLists.get(i).getSnippet().getChannelId();
+            YouTube.Videos.List videoRequest =
+                    youtube.videos().list("id,contentDetails,snippet");
 
-                }
+            videoRequest.setKey(requestKey);
+            videoRequest.setId(videoId);
+            videoRequest.setMaxResults(10l);
+            List<Video> videos = videoRequest.execute().getItems();
+            if(videos != null) {
+                // lecture 추가
             }
-
-            return playLists.toString();
+            return videos;
 
         } catch (GoogleJsonResponseException e) {
             System.err.println("There was a service error: " + e.getDetails().getCode() + " : "
