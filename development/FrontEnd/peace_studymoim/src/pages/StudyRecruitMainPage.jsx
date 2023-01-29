9ba18577c3f8f28453f4c427331600199f785bef
chaedev3 <chaedev3@gmail.com>
2023-01-29 12:38:42 +0900

    Feat: 스터디 구인 메인 페이지 정리

--- development/FrontEnd/peace_studymoim/src/pages/StudyRecruitMainPage.jsx
+++ development/FrontEnd/peace_studymoim/src/pages/StudyRecruitMainPage.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import StudyRecruitList from "../components/studypages/StudyRecruitList";
 import { Link } from "react-router-dom";
+import MainSearch from "../components/mainpages/MainSearch";
+import Tag from "../components/overall/Tag";
 
 // TODO: 취소 버튼 구현 안함, 강좌 선택도 넣지 않았음
 export default function StudyRecruitMainAll() {
@@ -37,12 +39,59 @@ export default function StudyRecruitMainAll() {
   }
 
   return (
-    <div className="max-w-6xl mx-auto px-4">
-      <h1>스터디 모집 메인 페이지</h1>
-      <StudyRecruitList recruits={LoadedStudyRecruits} />
-      <Link to="/study/study_recruit_form" className="btn">
-        스터디 만들기
-      </Link>
+    <div className="max-w-6xl mx-auto px-4 flex flex-col justify-start items-center gap-[20px] mt-10">
+      <MainSearch />
+
+      <div className="w-full flex flex-col justify-between items-center mt-8">
+        {/* TODO: map으로 돌려서 데이터에있는거 다 출력해야함 인기태그를 백에서 주면 좋을듯 */}
+        <div className="grid gap-4 grid-cols-5 grid-flow-row auto-rows-auto">
+          <Tag />
+          <Tag />
+          <Tag />
+          <Tag />
+          <Tag />
+        </div>
+      </div>
+
+      <div className="w-full flex flex-col justify-between items-center">
+        <div className="grid gap-4 grid-cols-4 grid-flow-row auto-rows-auto">
+          <Tag />
+          <Tag />
+          <Tag />
+          <Tag />
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-start items-start">
+        <div className="flex justify-end items-center self-stretch  h-[92px] gap-2.5 px-2.5 pb-2.5 bg-white border-t border-r-[0.3px] border-b-0 border-l-[0.3px] border-black">
+          <div className="flex justify-start items-start  gap-[23px]">
+            <Link to="/study/study_recruit_form">
+              <div
+                className="px-8 py-[13px] rounded-[10px] bg-[#b1b2ff]/50 text-xl font-bold text-black hover:bg-[#b1b2ff]/90 hover:scale-95"
+                style={{ boxShadow: "0px 4px 4px 0 rgba(0,0,0,0.25)" }}
+              >
+                스터디 만들기
+              </div>
+            </Link>
+
+            <select
+              name="커뮤니티정렬"
+              className="pl-[20px] w-[150px] h-[40px] mt-[10px] bg-[#f2f2f2] cursor-pointer"
+              style={{ boxShadow: "0px 4px 4px 0 rgba(0,0,0,0.25)" }}
+            >
+              <option value="">정렬하기</option>
+              <option value="new">최신순</option>
+              <option value="old">오래된순</option>
+              <option value="big">조회높은순</option>
+              <option value="small">조회낮은순</option>
+            </select>
+          </div>
+        </div>
+      
+      <div className="flex flex-col justify-start items-start w-full border-x-[0.3px] border-b-[0.3px] border-black px-3">
+        <StudyRecruitList recruits={LoadedStudyRecruits} />;
+        </div>
+      </div>
     </div>
   );
 }
