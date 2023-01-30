d9f5d2f989a71fbcd362e4a17f03b8142639e864
cjfal2 <cjfal2@kakao.com>
2023-01-30 23:44:03 +0900

    Feat: 코스페이지 기본 구성

--- development/FrontEnd/peace_studymoim/src/components/mainpages/MainNotLogIn.jsx
+++ development/FrontEnd/peace_studymoim/src/components/mainpages/MainNotLogIn.jsx
@@ -3,8 +3,9 @@ import MainCourse from "./MainCourse";
 export default function MainNotLogIn() {
   return (
     <>
-      <p className="text-xl">실시간 인기 강좌</p>
-      <div className="flex gap-[100px]">
+      <p className="text-xl"># 실시간 인기 강좌</p>
+      <div className="grid md:grid-cols-4 gap-10 grid-cols-2">
+        <MainCourse/>
         <MainCourse/>
         <MainCourse/>
         <MainCourse/>
