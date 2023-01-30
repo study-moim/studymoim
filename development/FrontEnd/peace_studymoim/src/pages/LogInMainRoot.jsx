2e1b50f634a5ec47742648e3fe4940e9108d1c84
chaelin <chaedev3@gmail.com>
2023-01-30 16:35:52 +0900

    Feat: 일단 커밋

--- development/FrontEnd/peace_studymoim/src/pages/LogInMainRoot.jsx
+++ development/FrontEnd/peace_studymoim/src/pages/LogInMainRoot.jsx
@@ -1,12 +1,13 @@
 // TODO: 이 화면은 로그인된 애는 못오게 해야함
 import { Link } from "react-router-dom";
-import KakaoLogin from 'react-kakao-login';
 import { KAKAO_AUTH_URL } from "../../../OAuth";
-
+import { useLocation } from "react-router-dom";
 
 /** @function 로그인창 */
 export default function LogInMainRoot() {
-
+  const handleLogin = () => {
+    window.location.href = KAKAO_AUTH_URL;  
+  }; 
   return (
     <div className="max-w-6xl mx-auto px-4">
       <div className="flex flex-col justify-start items-center relative gap-[60px]">
@@ -16,9 +17,9 @@ export default function LogInMainRoot() {
           className="flex-grow-0 flex-shrink-0 w-[495px] h-[271px] object-cover"
         />
         
-        <a href={KAKAO_AUTH_URL}> 
-          로그인 고고싱 
-        </a> 
+        <button onClick={handleLogin}>
+          카카오로 바로 가즈아! 
+        </button>
 
         {/* TODO: 관심사 선택페이지 이동용 라우트라서 나중에 지워야함 */}
         <Link to="/choice" className="text-xl">
