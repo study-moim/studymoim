fa8c835ecd4c7bcfccedec8199663419f0c81c14
baebae <tjdus2033@navver.com>
2023-01-31 13:08:30 +0900

    Feat: 로그인 security 처리

--- development/Backend/studymoim/src/main/java/com/ssafy/peace/config/SecurityConfig.java
+++ development/Backend/studymoim/src/main/java/com/ssafy/peace/config/SecurityConfig.java
@@ -1,35 +1,69 @@
 package com.ssafy.peace.config;
 
+import com.ssafy.peace.service.UserService;
+import com.ssafy.peace.service.auth.JwtAuthenticationFilter;
+import com.ssafy.peace.service.auth.UserDetailService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailService userDetailService;
+
+    @Autowired
+    private UserService userService;
+
+    // Password 인코딩 방식에 BCrypt 암호화 방식 사용
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // DAO 기반으로 Authentication Provider를 생성
+    // BCrypt Password Encoder와 UserDetailService 구현체를 설정
+    @Bean
+    DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(this.userDetailService);
+        return daoAuthenticationProvider;
+    }
+
+    // DAO 기반의 Authentication Provider가 적용되도록 설정
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS).permitAll()
-                // .antMatchers("/auth/**").permitAll()
-                // 상기 API 외에는 모든 요청에 인증 필요
-                // .anyRequest().authenticated()
-                .and()
-                .cors()
-                .and()
-                .csrf().disable()
-                // 시큐리티는 기본적으로 세션을 사용하지만, 여기서는 세션을 사용하지 않기 때문에 세션 설정을 Stateless 로 설정
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .oauth2Login()
-                .userInfoEndpoint();
-        ;
+        http.httpBasic().disable()
+             .csrf().disable()
+             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 사용 하지않음
+             .and()
+             .oauth2Login()
+             .and()
+             .authorizeRequests()
+             .antMatchers(HttpMethod.OPTIONS).permitAll()
+//           .antMatchers("/api/v1/users/me").authenticated()       //인증이 필요한 URL과 필요하지 않은 URL에 대하여 설정
+//           .anyRequest().permitAll()
+             .and().cors();
+        http.addFilter(new JwtAuthenticationFilter(authenticationManager(), userService)); //HTTP 요청에 JWT 토큰 인증 필터를 거치도록 필터를 추가
     }
 }
--- development/Backend/studymoim/src/main/java/com/ssafy/peace/service/auth/KakaoAuthService.java
+++ development/Backend/studymoim/src/main/java/com/ssafy/peace/service/auth/KakaoAuthService.java
@@ -1,4 +1,4 @@
-package com.ssafy.peace.service;
+package com.ssafy.peace.service.auth;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
