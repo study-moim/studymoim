e8ffc875ca36d978a0181c09c9efd62ca76473c6
cjfal2 <cjfal2@kakao.com>
2023-01-31 21:05:00 +0900

    Fix: dev 심폐소생술2

--- development/FrontEnd/peace_studymoim/src/components/mainpages/MainNotLogIn.jsx
+++ development/FrontEnd/peace_studymoim/src/components/mainpages/MainNotLogIn.jsx
@@ -5,10 +5,10 @@ export default function MainNotLogIn() {
     <>
       <p className="text-xl"># 실시간 인기 강좌</p>
       <div className="grid md:grid-cols-4 gap-10 grid-cols-2">
+        {/* <MainCourse/>
         <MainCourse/>
         <MainCourse/>
-        <MainCourse/>
-        <MainCourse/>
+        <MainCourse/> */}
       </div>
     </>
   );
