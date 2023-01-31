1522d76b9071a0a3c553b4b2d1cc71c3ce4d0b06
정채린 <vjflxk10@naver.com>
2023-01-31 09:07:56 +0900

    Delete OAuth.jsx

--- development/FrontEnd/peace_studymoim/OAuth.jsx
+++
@@ -1,4 +0,0 @@
-const REST_API_KEY = '98268e53473ceb3e11dd6e609a5fa990'
-const REDIRECT_URI = 'http://localhost:8080/oauth/login'
-
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
