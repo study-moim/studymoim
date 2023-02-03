d23fdfa5ad7028a29dae477c73d52d9217aeb788
baebae <tjdus2033@navver.com>
2023-02-03 14:26:07 +0900

    Design: 채팅 아이템 위치 수정

--- development/FrontEnd/peace_studymoim/src/components/mailbox/MyChatting.jsx
+++ development/FrontEnd/peace_studymoim/src/components/mailbox/MyChatting.jsx
@@ -1,7 +1,7 @@
 export default function MyChatting() {
   return (
-    <div className="absolute right-0 mr-[340px] mt-2">
-      <div className="p-3 border rounded-lg text-sm max-w-xs">
+    <div className="mt-2 mr-2 grid justify-items-end">
+      <div className="p-3 text-right border rounded-lg text-sm max-w-xs">
       어머머머 나도 필요했답니다
       </div>
     </div>
