a817fd62163744bcb8821cda658da73ffb93a7d3
박서영 <manchott@naver.com>
2023-02-03 17:58:19 +0900

    Feat: Backend 유저 닉네임 변경 기능 개발중

--- development/Backend/studymoim/src/main/java/com/ssafy/peace/dto/UserDto.java
+++ development/Backend/studymoim/src/main/java/com/ssafy/peace/dto/UserDto.java
@@ -41,25 +41,13 @@ public class UserDto {
 
     @Data
     @Builder
-    public static class Register {
-        @Size(min=5, max=50, message = "바르지 않은 email 크기 입니다")
-        @NotEmpty(message="email은 빈값 일 수 없습니다")
-        @NotNull(message="email은 null 일 수 없습니다")
-        @Pattern(regexp="^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$",
-                message = "바르지 않은 email 형식입니다")
-        private String email;
-        @Size(min=8, max=20, message = "바르지 않은 password 크기 입니다")
-        @NotEmpty(message="password는 빈값 일 수 없습니다")
-        @NotNull(message="password는 null 일 수 없습니다")
-        @Pattern(regexp="^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
-                message = "바르지 않은 password 형식입니다")
-        private String password;
+    public static class Nickname {
         @Size(min=2, max=6, message = "바르지 않은 nickname 크기 입니다")
         @NotEmpty(message="nickname은 빈값 일 수 없습니다")
         @NotNull(message="nickname은 null 일 수 없습니다")
         private String nickname;
         @Size(max = 255)
-        private String saveName;
+        private int userId;
     }
 
     @Data
