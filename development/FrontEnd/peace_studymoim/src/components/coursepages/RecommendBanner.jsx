55c30471e8182ff99c233baaab9ebc886e46722e
chaedev3 <chaedev3@gmail.com>
2023-02-04 20:34:18 +0900

    Feat: 일단 커밋

--- development/FrontEnd/peace_studymoim/src/components/coursepages/RecommendBanner.jsx
+++ development/FrontEnd/peace_studymoim/src/components/coursepages/RecommendBanner.jsx
@@ -12,30 +12,30 @@ export default function RecommendBanner() {
   console.log(recommendCourse);
 
   return (
-    <div className="flex justify-start items-start bg-[#ebefff]">
-      <div className="flex flex-col justify-center items-center flex-grow-0 flex-shrink-0 h-[648px] w-[720px] relative gap-[45px] pl-[150px] pr-[30px] py-[30px]">
-        <p className="flex-grow-0 flex-shrink-0 w-[720px] text-[82px] font-bold text-left">
-          <span className="flex-grow-0 flex-shrink-0 w-[720px] text-[82px] font-bold text-left text-[#a259ff]">
+    <div className="flex justify-center items-center bg-[#ebefff]">
+      <div className="flex flex-col justify-center items-center flex-grow-0 flex-shrink-0 w-6/12 relative gap-[45px] pl-[150px] pr-[30px] py-[30px]">
+        <p className="flex-grow-0 flex-shrink-0 w-full text-[60px] font-bold text-left">
+          <span className="flex-grow-0 flex-shrink-0 w-full font-bold text-left text-[#a259ff]">
             닉네임!
           </span>
-          <span className="flex-grow-0 flex-shrink-0 w-[720px] text-[82px] font-bold text-left text-black">
+          <span className="flex-grow-0 flex-shrink-0 w-full font-bold text-left text-black">
             님과
           </span>
           <br />
-          <span className="flex-grow-0 flex-shrink-0 w-[720px] text-[82px] font-bold text-left text-black">
+          <span className="flex-grow-0 flex-shrink-0 w-full font-bold text-left text-black">
             딱 맞는 강좌
           </span>
         </p>
-        <p className="flex-grow-0 flex-shrink-0 w-[720px] text-left text-[#58595d]">
-          <span className="flex-grow-0 flex-shrink-0 w-[720px] text-2xl font-bold text-left text-[#58595d]">
+        <p className="flex-grow-0 flex-shrink-0 w-full text-left text-[#58595d]">
+          <span className="flex-grow-0 flex-shrink-0 w-full text-2xl font-bold text-left text-[#58595d]">
             {recommendCourse && recommendCourse.title}
           </span>
           <br />
-          <span className="flex-grow-0 flex-shrink-0 w-[720px] text-xl text-left text-[#58595d]">
+          <span className="flex-grow-0 flex-shrink-0 w-full text-xl text-left text-[#58595d]">
             {recommendCourse && recommendCourse.courseProviderName}
           </span>
         </p>
-        <div className="flex justify-start items-center flex-grow-0 flex-shrink-0 w-[720px] gap-10 pr-[349.1875px]">
+        <div className="flex justify-start items-center flex-grow-0 flex-shrink-0 w-full gap-10 pr-[349.1875px]">
           <div
             className="flex justify-start items-start flex-grow-0 flex-shrink-0 relative pl-[65px] pr-[66.046875px] py-[22px] rounded-[15px] bg-[#ff7262] border-2 border-[#2e2f35]"
             style={{ boxShadow: "3px 3px 0px 0 #2e2f35" }}
@@ -58,6 +58,13 @@ export default function RecommendBanner() {
             )}
           </div>
         </div>
+      </div>
+      <div className="flex w-6/12 justify-center items-center flex-grow-0 flex-shrink-0 ">
+        {recommendCourse ? 
+          <img src={recommendCourse.thumbnail} alt="" className="w-full" />
+          :
+          <img src="" alt="" /> 
+        }
       </div>
     </div>
   );
