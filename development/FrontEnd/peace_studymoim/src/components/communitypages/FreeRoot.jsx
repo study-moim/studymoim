a39bbd6cbaac8f6f3835114a39fa5b57fa75f835
baebae <tjdus2033@navver.com>
2023-02-08 17:14:29 +0900

    Feat: 커뮤니티 페이지 검색과 정렬 기능 추가, 디자인 수정

--- development/FrontEnd/peace_studymoim/src/components/communitypages/FreeRoot.jsx
+++ development/FrontEnd/peace_studymoim/src/components/communitypages/FreeRoot.jsx
@@ -3,29 +3,78 @@ import getArticleList from "../../zustand/articles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import FreeQuestion from "./FreeQuestion";
+import { useState, useEffect } from "react";
 
 export default function FreeRoot() {
   getArticles();
   const { articles } = getArticleList();
   const freeArticles = articles;
+  const [word, setWord] = useState("");
+  const [selected, setSelected] = useState("new");
+  const [filterInfo, setFilterInfo] = useState(freeArticles);
+
+  useEffect(() => {
+    const getWord = async () => {
+      setFilterInfo(
+        freeArticles.filter((free) => {
+          return free.title
+            .replace(" ", "")
+            .toLocaleLowerCase()
+            .includes(word.toLocaleLowerCase().replace(" ", ""));
+        })
+      );
+    };
+    getWord();
+  }, [word]);
+
+  useEffect(() => {
+    const getSelected = async () => {
+      if (selected == "new") {
+        setFilterInfo(freeArticles);
+      } else if (selected == "old") {
+        setFilterInfo(freeArticles.reverse());
+      } else if (selected == "big") {
+        setFilterInfo(
+          freeArticles.sort((prev, cur) => {
+            if (prev.hit > cur.hit) return 1;
+            if (prev.hit < cur.hit) return -1;
+          })
+        );
+      } else if(selected == "small"){
+        setFilterInfo(
+          freeArticles.sort((prev, cur) => {
+            if (prev.hit < cur.hit) return 1;
+            if (prev.hit > cur.hit) return -1;
+          })
+        );
+      }
+    };
+    getSelected();
+  }, [selected]);
+
   return (
     <div className="w-full">
-      <div className="flex justify-center items-center self-stretch h-[100px] gap-2.5">
-        <form className="flex relative h-[42px] border border-slate-500 bg-white rounded-[30px] w-[400px]">
-          <button className="absolute right-0 bg-[#B1B2FF] rounded-full w-[30px] h-[30px] my-[5px] mr-[5px] text-white">
+      <div className="flex justify-end items-center self-stretch h-[100px] gap-2.5">
+      <form className="flex relative h-[50px] w-[400px]">
+          <button className="absolute right-0 bg-[#B1B2FF] rounded-full w-[30px] h-[30px] m-[10px] text-white">
             <FontAwesomeIcon icon={faMagnifyingGlass} />
           </button>
           <input
             type="text"
             placeholder="궁금한 자유글을 검색해보세요!"
-            className="w-full rounded-[30px] pl-4"
+            className="w-full h-[50px] border border-slate-500 rounded-[30px] pl-4 focus:outline-none focus:ring focus:ring-violet-300"
+            onChange={(e) => {
+              setWord(e.target.value);
+            }}
           />
         </form>
         <select
           name="커뮤니티정렬"
-          className="px-[20px] w-[150px] h-[42px] border border-slate-500 rounded-[30px] cursor-pointer"
+          className="px-[20px] w-[150px] h-[50px] border border-slate-500 rounded-[30px] cursor-pointer"
+          onChange={(e) => {
+            setSelected(e.target.value);
+          }}
         >
-          <option value="">정렬하기</option>
           <option value="new">최신순</option>
           <option value="old">오래된순</option>
           <option value="big">조회높은순</option>
@@ -33,7 +82,7 @@ export default function FreeRoot() {
         </select>
       </div>
       <div className="flex flex-col justify-start items-start w-full">
-        {freeArticles.map((freeArticle) => (
+        {filterInfo.map((freeArticle) => (
           <FreeQuestion
             key={freeArticle.freeBoardId}
             freeArticle={freeArticle}
