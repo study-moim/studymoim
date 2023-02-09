c96bb1732152f055e40b948cad5e07aeaf188065
baebae <tjdus2033@navver.com>
2023-02-09 17:37:21 +0900

    Modify: 스터디 검색 기능 수정, 디자인 수정

--- development/FrontEnd/peace_studymoim/src/components/studypages/StudySearch.jsx
+++ development/FrontEnd/peace_studymoim/src/components/studypages/StudySearch.jsx
@@ -10,42 +10,14 @@ export default function StudySearch() {
   const studyRecruits = useFetch(`http://${API_SERVER}/api/v1/study/`);
   const tags = useFetch(`http://${API_SERVER}/api/v1/category/`);
   const [word, setWord] = useState("");
-  const [searchType, setSearchType] = useState("");
-  const [filterInfo, setFilterInfo] = useState(studyRecruits);
 
-  useEffect(() => {
-    const getWord = async () => {
-      if (searchType == "word") {
-        setFilterInfo(
-          studyRecruits.filter((study) => {
-            return study.title
-              .replace(" ", "")
-              .toLocaleLowerCase()
-              .includes(word.toLocaleLowerCase().replace(" ", ""));
-          })
-        );
-      } else if (searchType == "tag") {
-        const getTagSearchStudy = async () => {
-          await fetch(`http://${API_SERVER}/api/v1/study/category/${word}`)
-            .then((res) => res.json())
-            .then((json) => {
-              setFilterInfo(json);
-            });
-        };
-        getTagSearchStudy();
-      } else {
-        const getStudyRecruits = async () => {
-          await fetch(`http://${API_SERVER}/api/v1/study/`)
-            .then((res) => res.json())
-            .then((json) => {
-              setFilterInfo(json);
-            });
-        };
-        getStudyRecruits();
-      }
-    };
-    getWord();
-  }, [word]);
+  let filterInfo = studyRecruits.filter((study) => {
+    return study.title
+      .concat(study.content)
+      .replace(" ", "")
+      .toLocaleLowerCase()
+      .includes(word.toLocaleLowerCase().replace(" ", ""));
+  });
 
   return (
     <div className="w-full">
@@ -60,7 +32,6 @@ export default function StudySearch() {
               placeholder="찾고 있는 스터디를 검색하세요!"
               className="w-full h-[40px] text-[14px] border border-slate-500 rounded-[30px] pl-4 focus:outline-none focus:ring focus:ring-violet-300 "
               onChange={(e) => {
-                setSearchType("word");
                 setWord(e.target.value);
               }}
             />
@@ -68,42 +39,13 @@ export default function StudySearch() {
           {/* TODO: 정렬도 해야 됨 원래는 백에서 주기로 한듯한데.. 우리가 해야될듯? ㅋ  */}
         </div>
       </div>
-      <div className="flex flex-col justify-start items-start w-full">
-        <div className="w-full flex flex-col">
-          <p className="text-base"># 태그 검색</p>
-          <div className="flex flex-row flex-wrap gap-2 mt-3">
-            <button
-              className={
-                "hover:bg-gray-200 min-w-[80px] w-fit flex flex-col justify-center items-center rounded-[10px] px-3 py-1 border "
-              }
-              onClick={async () => {
-                setSearchType("");
-                setWord("");
-              }}
-            >
-              <p className="text-[14px]">전체</p>
-            </button>
-            {tags.map((tag) => (
-              <div
-                key={tag.courseCategoryId}
-                onClick={async () => {
-                  setSearchType("tag");
-                  setWord(tag.courseCategoryId);
-                }}
-              >
-                <CourseTag tag={tag} />
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="flex flex-row flex-wrap gap-3 w-full mt-10">
+        <div className="flex flex-row flex-wrap justify-between gap-1 mt-10 w-full">
           {filterInfo.map((recruit) => (
-            <div key={recruit.studyId} className="cursor-pointer w-[12/12]">
+            <div key={recruit.studyId} className="cursor-pointer min-w-[33%]">
               <StudyRecruitItem props={recruit} />
             </div>
           ))}
         </div>
-      </div>
     </div>
   );
 }
