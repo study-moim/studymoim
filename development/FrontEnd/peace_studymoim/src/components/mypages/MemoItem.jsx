1a6738ad2dd94a991b3336f4e98e7ca058330535
baebae <tjdus2033@navver.com>
2023-02-13 13:06:25 +0900

    Feat: 메모, 스터디, 강좌 강의 기능 완!

--- development/FrontEnd/peace_studymoim/src/components/mypages/MemoItem.jsx
+++ development/FrontEnd/peace_studymoim/src/components/mypages/MemoItem.jsx
@@ -4,22 +4,20 @@ import MemoModal from "./MemoModal";
 import Moment from "moment/moment";
 import "moment/locale/ko";
 
-export default function MemoLecture({ lectureData, getPageName }) {
+export default function MemoLecture({ lectureData, userId }) {
   const lectureId = lectureData.lectureId;
   const API_SERVER = import.meta.env.VITE_APP_API_SERVER;
-  // Todo 메모 api 바꾸기
-  // const memoInfo = useFetch(`http://${API_SERVER}/api/v1/note/${lectureId}/${getPageName}`);
-  const memoInfo = useFetch(`http://${API_SERVER}/api/v1/note/${lectureId}/1`);
+  const memoInfo = useFetch(`http://${API_SERVER}/api/v1/note/${lectureId}/${userId}`);
   const slicedTitle = lectureData.title.substring(0, 40) + "...";
   const [showModal, setShowModal] = useState(false);
 
   function closeModalHandler() {
-    setShowModal(false);
+    setShowModal(!showModal);
   }
 
   return (
     <div
-      onClick={() => setShowModal(true)}
+      onClick={() => setShowModal(!showModal)}
       className="flex justify-start items-center relative p-[10px] mx-[20px] rounded-[15px] bg-white border-[1px] border-[#eef1ff]/[0.98] cursor-pointer mt-1 hover:border-[#7b61ff]"
     >
       <p className="text-[13px] min-w-[60%] max-w-[60%]">
@@ -36,7 +34,7 @@ export default function MemoLecture({ lectureData, getPageName }) {
           key={memoInfo.noteId}
           lectureData={lectureData}
           memoData={memoInfo}
-          onCancel={() => closeModalHandler()}
+          onCancel={closeModalHandler}
         />
       ) : null}
     </div>
--- development/FrontEnd/peace_studymoim/src/components/mypages/MemoLecture.jsx
+++ development/FrontEnd/peace_studymoim/src/components/mypages/MemoLecture.jsx
@@ -3,9 +3,8 @@ import MemoItem from "./MemoItem";
 
 export default function MemoLecture({ courseId, getPageName }) {
   const API_SERVER = import.meta.env.VITE_APP_API_SERVER;
-  // Todo 메모 api 바꾸기
-  // const lectureInfo = useFetch(`http://${API_SERVER}/api/v1/note/lecture/${getPageName}/${courseId}`);
-  const lectureInfo = useFetch(`http://${API_SERVER}/api/v1/note/lecture/1/${courseId}`);
+  const [lectureInfo, setLectureInfo] = useState([]);
+  useFetch(`http://${API_SERVER}/api/v1/note/lecture/${getPageName}/${courseId}`);
 
   return (
     <>
