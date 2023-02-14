e747c7653c233fc0fe26f927c6012823201d07a3
chaedev3 <chaedev3@gmail.com>
2023-02-14 20:49:57 +0900

    Feat: 비로그인 메인창

---
+++ development/FrontEnd/peace_studymoim/src/components/mainpages/MainLectureQuestion.jsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+
+export default function MainLectureQuestion({ propData }) {
+  const IMAGE_ROOT = import.meta.env.VITE_APP_IMAGE_ROOT;
+  const image = IMAGE_ROOT + propData.user.saveName;
+  console.log(propData) 
+  if (propData.content.length >= 30) {
+    return propData.content.substr(0, 30) + "..."; 
+  }
+  
+  return (
+    <Link
+      to={`community/question/${propData.questionBoardId}`}
+      state={{
+        propData: propData,
+      }}
+    >
+      <div className="min-w-[350px] max-w-[350px] h-[150px] rounded-[15px] border p-5 cursor-pointer gap-2 transition ease-in-out duration-300 hover:-translate-y-1.5 hover:shadow-md">
+        <div className="flex flex-col gap-2 h-[150px]">
+          <p className="text-[15px] font-bold">{propData.title}</p>
+          <p className="text-[13px] text-gray-500">{propData.content}</p>
+          <p className="text-[13px] text-gray-500">{propData.lecture.title}</p>
+          <div className="flex justify-start items-center">
+            <img
+              className="w-10 rounded-full border mr-3"
+              src={propData.user.saveName ? image : "/logo.png"}
+            />
+            <p className="text-[13px] text-[#B1B2FF]">
+              {propData.user.nickname}
+            </p>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
--- development/FrontEnd/peace_studymoim/src/components/mainpages/MainNotLogIn.jsx
+++ development/FrontEnd/peace_studymoim/src/components/mainpages/MainNotLogIn.jsx
@@ -6,13 +6,23 @@ import "slick-carousel/slick/slick-theme.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router";
+import MainStudy from "./MainStudy";
+import MainFreeArticle from "./MainFreeArticle";
+import MainLectureQuestion from "./MainLectureQuestion";
 
 export default function MainNotLogIn({ searchKey, searchData }) {
   const navigate = useNavigate();
+
   const API_SERVER = import.meta.env.VITE_APP_API_SERVER;
-  const courseInfo = useFetch(`http://${API_SERVER}/api/v1/course/`); 
-  // const freeArticleInfo = useFetch(`http://${API_SERVER}/api/v1/free`); 
-  // const courseArticleInfo = useFetch(``); 
+  const courseInfo = useFetch(`http://${API_SERVER}/api/v1/course/`);
+  const studyInfo = useFetch(`http://${API_SERVER}/api/v1/study/`);
+  const freeArticleInfo = useFetch(
+    `http://${API_SERVER}/api/v1/articles/free`
+  ).content;
+  const courseArticleInfo = useFetch(
+    `http://${API_SERVER}/api/v1/articles/question`
+  ).content;
+
   const SlickButtonFix = ({ currentSlide, slideCount, children, ...props }) => (
     <span {...props}>{children}</span>
   );
@@ -37,6 +47,46 @@ export default function MainNotLogIn({ searchKey, searchData }) {
     autoplaySpeed: 3000,
     responsive: [
       {
+        breakpoint: 1080,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 830,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
+  const twoSettings = {
+    dots: false,
+    infinite: true,
+    arrows: true,
+    prevArrow: (
+      <SlickButtonFix>
+        <img src="/left-arrow.png" alt="" />
+      </SlickButtonFix>
+    ),
+    nextArrow: (
+      <SlickButtonFix>
+        <img src="/right-arrow.png" alt="" />
+      </SlickButtonFix>
+    ),
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
         breakpoint: 960,
         settings: {
           slidesToShow: 2,
@@ -51,23 +101,23 @@ export default function MainNotLogIn({ searchKey, searchData }) {
     ],
   };
 
-  let filterInfo = courseInfo.filter((course) => {
-    if (searchKey == "word") {
-      return course.title
-        .replace(" ", "")
-        .toLocaleLowerCase()
-        .includes(searchData.toLocaleLowerCase().replace(" ", ""));
-    } else if (searchKey == "tag") {
-      if (
-        course.categoryList.length != 0 &&
-        course.categoryList[0].courseCategoryId == searchData
-      ) {
-        return course;
-      }
-    } else {
-      return courseInfo;
-    }
-  });
+  // let filterInfo = courseInfo.filter((course) => {
+  //   if (searchKey == "word") {
+  //     return course.title
+  //       .replace(" ", "")
+  //       .toLocaleLowerCase()
+  //       .includes(searchData.toLocaleLowerCase().replace(" ", ""));
+  //   } else if (searchKey == "tag") {
+  //     if (
+  //       course.categoryList.length != 0 &&
+  //       course.categoryList[0].courseCategoryId == searchData
+  //     ) {
+  //       return course;
+  //     }
+  //   } else {
+  //     return courseInfo;
+  //   }
+  // });
 
   return (
     <>
@@ -76,37 +126,132 @@ export default function MainNotLogIn({ searchKey, searchData }) {
         <FontAwesomeIcon
           icon={faCircleArrowRight}
           onClick={() => {
-            navigate('/Course');
+            navigate("/Course");
           }}
         />
       </div>
 
-      <Slider {...settings}>
+      {/* <Slider {...settings}>
         {filterInfo.map((course) => (
           <div key={course.course_id}>
             <MainCourse key={course.course_id} propData={course} />
           </div>
         ))}
-      </Slider>
-
-      {/* <div className="flex justify-start items-center">
-        <p className="text-lg text-left font-bold my-5 mr-3"># 자유 게시글</p>
-        <FontAwesomeIcon
-          icon={faCircleArrowRight}
-          onClick={() => {
-            navigate('/community');
-          }}
-        />
-      </div>
-
-      <Slider {...settings}>
-        {freeArticleInfo.map((course) => (
+      </Slider> */}
+      {courseInfo.length > 3 ? (
+        <Slider {...settings}>
+        {courseInfo.map((course) => (
           <div key={course.course_id}>
             <MainCourse key={course.course_id} propData={course} />
           </div>
         ))}
-      </Slider> */}
+      </Slider>
+      ): (
+        <div>
+           {courseInfo.map((course) => (
+          <div key={course.course_id}>
+            <MainCourse key={course.course_id} propData={course} />
+          </div>
+        ))}
+        </div>
+      ) }
+      
 
+      <div className="mb-5">
+        <div className="flex justify-start items-center">
+          <p className="text-lg text-left font-bold my-5 mr-3 mt-5">
+            # 모집 중인 스터디
+          </p>
+          <FontAwesomeIcon
+            icon={faCircleArrowRight}
+            onClick={() => {
+              navigate("/study");
+            }}
+          />
+        </div>
+
+        {studyInfo.length > 2 ? (
+          <Slider {...twoSettings}>
+            {studyInfo.map((study) => (
+              <div key={study.studyId}>
+                <MainStudy key={study.studyId} propData={study} />
+              </div>
+            ))}
+          </Slider>
+        ) : (
+          <div>
+            {studyInfo.map((study) => (
+              <div key={study.studyId}>
+                <MainStudy key={study.studyId} propData={study} />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+
+      <div className="flex justify-start items-center">
+        <p className="text-lg text-left font-bold my-5 mr-3"># 자유 질문</p>
+        <FontAwesomeIcon
+          icon={faCircleArrowRight}
+          onClick={() => {
+            navigate("/community");
+          }}
+        />
+      </div>
+
+      {freeArticleInfo.length > 2 ? (
+        <Slider {...twoSettings}>
+          {freeArticleInfo.map((free) => (
+            <div key={free.freeBoardId}>
+              <MainFreeArticle key={free.freeBoardId} propData={free} />
+            </div>
+          ))}
+        </Slider>
+      ) : (
+        <div>
+          {freeArticleInfo.map((free) => (
+            <div key={free.freeBoardId}>
+              <MainFreeArticle key={free.freeBoardId} propData={free} />
+            </div>
+          ))}
+        </div>
+      )}
+
+      <div className="flex justify-start items-center">
+        <p className="text-lg text-left font-bold my-5 mr-3"># 강의 질문</p>
+        <FontAwesomeIcon
+          icon={faCircleArrowRight}
+          onClick={() => {
+            navigate("/community");
+          }}
+        />
+      </div>
+
+      {courseArticleInfo.length > 2 ? (
+        <Slider {...twoSettings}>
+          {courseArticleInfo.map((course) => (
+            <div key={course.questionBoardId}>
+              <MainLectureQuestion
+                key={course.questionBoardId}
+                propData={course}
+              />
+            </div>
+          ))}
+        </Slider>
+      ) : (
+        <div>
+          {courseArticleInfo.map((course) => (
+            <div key={course.questionBoardId}>
+              <MainLectureQuestion
+                key={course.questionBoardId}
+                propData={course}
+              />
+            </div>
+          ))}
+        </div>
+      )}
+
+      <div className="my-5"></div>
     </>
   );
 }
