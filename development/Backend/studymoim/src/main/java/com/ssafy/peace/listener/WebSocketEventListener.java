53f6403646afee9efeea54c0ef5f9c706e780d12
junho <blacktick@naver.com>
2023-02-14 17:54:56 +0900

    Feat: update로 변경

--- development/Backend/studymoim/src/main/java/com/ssafy/peace/listener/WebSocketEventListener.java
+++ development/Backend/studymoim/src/main/java/com/ssafy/peace/listener/WebSocketEventListener.java
@@ -3,19 +3,24 @@ package com.ssafy.peace.listener;
 import com.ssafy.peace.dto.ChatMessageDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.simp.user.SimpSubscriptionMatcher;
 import org.springframework.messaging.simp.user.SimpUserRegistry;
+import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Slf4j
-@Component
-public class WebSocketEventListener {
+@Configurable
+public class WebSocketEventListener implements ChannelInterceptor {
 
     @Autowired
     private SimpMessageSendingOperations messagingTemplate;
@@ -23,8 +28,11 @@ public class WebSocketEventListener {
     private SimpUserRegistry simpUserRegistry;
 
     @EventListener
-    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
-        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+    public void handleWebSocketConnectListener(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        if(StompCommand.SUBSCRIBE == accessor.getCommand()) {
+
+        }
     }
 
     @EventListener
