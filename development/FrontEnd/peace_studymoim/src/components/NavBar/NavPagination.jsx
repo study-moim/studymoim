ee164c9af1fcb466ab3df3b4c4c61bd9af901e22
cjfal2 <cjfal2@kakao.com>
2023-02-15 13:05:15 +0900

    Feat: key에러 수정 + 플레이어 퀘스쳔 pre 적용

--- development/FrontEnd/peace_studymoim/src/components/NavBar/NavPagination.jsx
+++ development/FrontEnd/peace_studymoim/src/components/NavBar/NavPagination.jsx
@@ -29,9 +29,9 @@ export default function NavPagination({
       }
     } else if (pageCount - pageRangeDisplayed / 2 < currentPage) {
       console.log("끝");
-      console.log(pageCount, pageRangeDisplayed)
+      console.log(pageCount, pageRangeDisplayed);
       for (let i = pageCount - pageRangeDisplayed; i < pageCount; i++) {
-        if(i < 0) continue;
+        if (i < 0) continue;
         newDisplay.push(i + 1);
       }
     } else {
@@ -41,42 +41,59 @@ export default function NavPagination({
         i < Math.floor(currentPage + pageRangeDisplayed / 2);
         i++
       ) {
-        if(i < 0) continue;
+        if (i < 0) continue;
         newDisplay.push(i + 1);
       }
     }
     setPageDisplay(newDisplay);
   }
 
-    return(
-        <nav aria-label="Page navigation example" className="flex justify-center mt-3">
-            <ul className="inline-flex space-x-2">
-                <li>
-                    <div onClick={() => onPageClick(1)}
-                        className="px-3 py-2 ml-0 leading-tight rounded-full hover:bg-gray-100 dark:hover:text-white cursor-pointer">
-                        {firstLabel}
-                    </div>
-                </li>
-                {pageDisplay.filter((idx) => idx<=pageCount).map((idx) => (
-                    idx==currentPage ? (<li>
-                        <div onClick={() => onPageClick(idx)}
-                            className="px-3 py-2 leading-tight rounded-full bg-violet-100 cursor-pointer">
-                            {idx}
-                        </div>
-                    </li>) : (<li>
-                        <div onClick={() => onPageClick(idx)}
-                            className="px-3 py-2 leading-tight rounded-full bg-white  hover:bg-gray-100 cursor-pointer">
-                            {idx}
-                        </div>
-                    </li>)
-                ))}
-                <li>
-                    <div onClick={() => onPageClick(pageCount)}
-                        className="px-3 py-2 leading-tight rounded-full hover:bg-gray-100 cursor-pointer">
-                        {lastLabel}
-                    </div>
-                </li>
-            </ul>
-        </nav> 
-    )
+  return (
+    <nav
+      aria-label="Page navigation example"
+      className="flex justify-center mt-3"
+    >
+      <ul className="inline-flex space-x-2">
+        <li>
+          <div
+            onClick={() => onPageClick(1)}
+            className="px-3 py-2 ml-0 leading-tight rounded-full hover:bg-gray-100 dark:hover:text-white cursor-pointer"
+          >
+            {firstLabel}
+          </div>
+        </li>
+        {pageDisplay
+          .filter((idx) => idx <= pageCount)
+          .map((idx) =>
+            idx == currentPage ? (
+              <li key={idx}>
+                <div
+                  onClick={() => onPageClick(idx)}
+                  className="px-3 py-2 leading-tight rounded-full bg-violet-100 cursor-pointer"
+                >
+                  {idx}
+                </div>
+              </li>
+            ) : (
+              <li key={idx}>
+                <div
+                  onClick={() => onPageClick(idx)}
+                  className="px-3 py-2 leading-tight rounded-full bg-white  hover:bg-gray-100 cursor-pointer"
+                >
+                  {idx}
+                </div>
+              </li>
+            )
+          )}
+        <li>
+          <div
+            onClick={() => onPageClick(pageCount)}
+            className="px-3 py-2 leading-tight rounded-full hover:bg-gray-100 cursor-pointer"
+          >
+            {lastLabel}
+          </div>
+        </li>
+      </ul>
+    </nav>
+  );
 }
