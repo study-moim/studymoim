e4fb8df3e3629784a4e842b83b1b2d557df91df0
cjfal2 <cjfal2@kakao.com>
2023-02-15 17:45:29 +0900

    Feat: 강좌 디테일 페이지네이션 위로 올림

--- development/FrontEnd/peace_studymoim/src/pages/CourseDetailRoot.jsx
+++ development/FrontEnd/peace_studymoim/src/pages/CourseDetailRoot.jsx
@@ -6,7 +6,10 @@ import StudyShort from "../components/coursedetail/StudyShort";
 import CourseQuestion from "../components/coursedetail/CourseQuestion";
 import NavPagination from "../components/NavBar/NavPagination.jsx";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons"; 
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 import userInfo from "../zustand/store";
 import LoginModal from "../components/NavBar/LoginModal";
 
@@ -15,14 +18,14 @@ export default function CourseDetailRoot() {
   function closeModalHandler() {
     setShowModal(false);
   }
-  
+
   const { info } = userInfo();
   useEffect(() => {
     if (!info) {
       setShowModal(true);
     }
   });
-  
+
   const API_SERVER = import.meta.env.VITE_APP_API_SERVER;
   const props = useLocation().state.propData;
   const [currentClick, setCurrentClick] = useState("curriculum");
@@ -42,41 +45,49 @@ export default function CourseDetailRoot() {
     likeUserCount: props.likeUserCount,
   });
   const [params, setParams] = useState({
-    "key": "title",
-    "word": "",
-    "page": 0,
-    "size": 5
+    key: "title",
+    word: "",
+    page: 0,
+    size: 5,
   });
 
   useEffect(() => {
     updateBannerLecture();
-  }, [])
+  }, []);
 
   useEffect(() => {
     const getPage = async (url) => {
       let query = Object.keys(params)
-          .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
-          .join('&');
-      if(currentClick == 'curriculum') {}
-      else if(currentClick == 'study') query = query + '&sort=creationTime,desc'
-      else if(currentClick == 'community') query = query + '&sort=publishTime,desc'
-      let resp = await fetch(`${url}?${query}`)
-      let data = await resp.json()
+        .map((k) => encodeURIComponent(k) + "=" + encodeURIComponent(params[k]))
+        .join("&");
+      if (currentClick == "curriculum") {
+      } else if (currentClick == "study")
+        query = query + "&sort=creationTime,desc";
+      else if (currentClick == "community")
+        query = query + "&sort=publishTime,desc";
+      let resp = await fetch(`${url}?${query}`);
+      let data = await resp.json();
       setPage(data);
     };
-    if(currentClick=='curriculum'){
+    if (currentClick == "curriculum") {
       getPage(`http://${API_SERVER}/api/v1/lecture/${props.course_id}`);
     }
-    if(currentClick=='study') {
-      getPage(`http://${API_SERVER}/api/v1/course/${props.course_id}/study_list`);
+    if (currentClick == "study") {
+      getPage(
+        `http://${API_SERVER}/api/v1/course/${props.course_id}/study_list`
+      );
     }
-    if(currentClick=='community') {
-      getPage(`http://${API_SERVER}/api/v1/articles/question/course/${props.course_id}`);
+    if (currentClick == "community") {
+      getPage(
+        `http://${API_SERVER}/api/v1/articles/question/course/${props.course_id}`
+      );
     }
   }, [params]);
 
   const updateBannerLecture = async () => {
-    let resp = await fetch(`http://${API_SERVER}/api/v1/lecture/${props.course_id}?page=0&size=10000`);
+    let resp = await fetch(
+      `http://${API_SERVER}/api/v1/lecture/${props.course_id}?page=0&size=10000`
+    );
     let data = await resp.json();
     let tot = 0;
     data.content.forEach((lecture) => {
@@ -98,7 +109,10 @@ export default function CourseDetailRoot() {
     setCurrentClick(event.target.id);
   };
 
-  useEffect((e) => {
+  useEffect(
+    (e) => {
+      setPage(null);
+      setCurrentPage(0);
       if (currentClick !== null) {
         let current = document.getElementById(currentClick);
         current.style.backgroundColor = "#8871f9";
@@ -109,10 +123,10 @@ export default function CourseDetailRoot() {
       }
       setPrevClick(currentClick);
       setParams({
-        "key": "title",
-        "word": "",
-        "page": 0,
-        "size": 5
+        key: "title",
+        word: "",
+        page: 0,
+        size: 5,
       });
     },
     [currentClick]
@@ -121,12 +135,12 @@ export default function CourseDetailRoot() {
   useEffect(() => {
     const getParams = () => {
       setParams({
-        "key": "title",
-        "word": word,
-        "page": currentPage-1,
-        "size": 5
-      })
-    }
+        key: "title",
+        word: word,
+        page: currentPage - 1,
+        size: 5,
+      });
+    };
     getParams();
   }, [word, currentPage]);
 
@@ -160,51 +174,58 @@ export default function CourseDetailRoot() {
             커뮤니티
           </div>
         </div>
-        <div className="flex flex-col justify-start items-center w-full h-fit border-t border-gray-200 overflow-auto">
+        <NavPagination
+          className="absolute"
+          firstLabel={<FontAwesomeIcon icon={faChevronLeft} />}
+          lastLabel={<FontAwesomeIcon icon={faChevronRight} />}
+          breakLabel="..."
+          onPageChange={setCurrentPage}
+          pageCount={page ? page.totalPages : 0}
+          pageRangeDisplayed={5}
+        />
+        <div className="flex flex-col justify-start items-center w-full h-fit border-gray-200 overflow-auto">
           {currentClick === "curriculum" ? (
-            <div className="w-full flex flex-col justify-center items-center gap-[15px] p-3">
-              {page ? page.content.map((lecture, idx) => (
-                <LectureShort
-                  key={lecture.lectureId}
-                  propData={lecture}
-                  lectureIndex={idx + 1}
-                  courseId={props.course_id}
-                />
-              )) : null}
+            <div className="w-full flex flex-col justify-center items-center gap-[15px] p-3 h-full">
+              {page
+                ? page.content.map((lecture, idx) => (
+                    <LectureShort
+                      key={lecture.lectureId}
+                      propData={lecture}
+                      lectureIndex={idx + 1}
+                      courseId={props.course_id}
+                    />
+                  ))
+                : null}
             </div>
           ) : null}
           {currentClick === "study" ? (
-            <div className="w-full flex flex-col justify-center items-center gap-[15px] p-3">
-              {page ? page.content.map((study, idx) => (
-                <StudyShort
-                  key={study.studyId}
-                  propData={study}
-                  studyIndex={idx + 1}
-                />
-              )) : null}
+            <div className="w-full flex flex-col justify-center items-center gap-[15px] p-3 h-full">
+              {page
+                ? page.content.map((study, idx) => (
+                    <StudyShort
+                      key={study.studyId}
+                      propData={study}
+                      studyIndex={idx + 1}
+                    />
+                  ))
+                : null}
             </div>
           ) : null}
           {currentClick === "community" ? (
-            <div className="w-full flex flex-col justify-center items-center gap-[15px] p-3">
-              {page ? page.content.map((question, idx) => (
-                <CourseQuestion
-                  key={question.questionBoardId}
-                  propData={question}
-                  courseId={props.course_id}
-                  questionIndex={idx + 1}
-                />
-              )) : null}
+            <div className="w-full flex flex-col justify-center items-center gap-[15px] p-3 h-full">
+              {page
+                ? page.content.map((question, idx) => (
+                    <CourseQuestion
+                      key={question.questionBoardId}
+                      propData={question}
+                      courseId={props.course_id}
+                      questionIndex={idx + 1}
+                    />
+                  ))
+                : null}
             </div>
           ) : null}
         </div>
-        <NavPagination
-            firstLabel={<FontAwesomeIcon icon={faChevronLeft}/>}
-            lastLabel={<FontAwesomeIcon icon={faChevronRight}/>}
-            breakLabel="..."
-            onPageChange={setCurrentPage}
-            pageCount={page ? page.totalPages : 0}
-            pageRangeDisplayed={5}
-        />
       </div>
       {showModal ? (
         <LoginModal
