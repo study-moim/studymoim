eb043c411a54227322c890d7227b1ffde9164fa2
chaelin <chaedev3@gmail.com>
2023-02-16 09:58:49 +0900

    Feat: 로그인 모달창으로 바꾸기 & 캐러셀 화살표 중앙으로 맞추기

--- development/FrontEnd/peace_studymoim/src/pages/StudyRecruitDetailPage.jsx
+++ development/FrontEnd/peace_studymoim/src/pages/StudyRecruitDetailPage.jsx
@@ -1,31 +1,24 @@
-import userInfo from "../zustand/store";
-import { useNavigate } from "react-router-dom";
 import { Link, NavLink } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import StudyRecruitModalNotOpen from "../components/studypages/StudyRecruitModalNotOpen";
 import StudyRecruitModalOpen from "../components/studypages/StudyRecruitModalOpen";
 import MainCourse from "../components/mainpages/MainCourse";
+import userInfo from "../zustand/store";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faCalendar,
-  faUser,
+  faCalendarDays,
+  faUsers,
   faCircleCheck,
-} from "@fortawesome/free-regular-svg-icons";
+} from "@fortawesome/free-solid-svg-icons";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./StudyRecruitDetailPage.css";
+import LoginModal from '../components/NavBar/LoginModal';    
 
 export default function StudyRecruitDetailPage(props) {
-  const navigate = useNavigate();
-  const { info } = userInfo();
-
-  useEffect(() => {
-    if (!info) {
-      navigate("/login");
-      return;
-    }
-  });
+  const [showModal, setShowModal] = useState(false);
   const [showOpenModal, setShowOpenModal] = useState(false);
   const [showNotOpenModal, setShowNotOpenModal] = useState(false);
   const studyId = useParams();
@@ -71,6 +64,12 @@ export default function StudyRecruitDetailPage(props) {
   const [studyDetail, setStudyDetail] = useState([]);
   const [userList, setUserList] = useState([]);
   const [curriculum, setCurriculum] = useState([]);
+  const { info } = userInfo();
+  useEffect(() => {
+    if (!info) {
+      setShowModal(true); 
+    }
+  });
 
   useEffect(() => {
     fetch(`http:///${API_SERVER}/api/v1/study/${detailId}`)
@@ -99,17 +98,20 @@ export default function StudyRecruitDetailPage(props) {
       setShowOpenModal(true);
     }
   }
-  if (userList.userId) 
+
+  function closeLoginHandler() {
+    setShowModal(false); 
+  };  
   return (
     <>
       <div className="flex flex-col justify-center items-center mt-[50px] max-w-6xl mx-auto px-4">
         <div className="flex flex-col w-9/12">
-          <div className="w-full pl-1 py-5 text-[25px] font-bold text-black">
+          <div className="w-full py-7 text-2xl font-bold text-black">
             {studyDetail.title}
           </div>
-          <div className="flex justify-start items-center relative pb-4 border-b">
+          <div className="flex justify-start items-center relative pb-7 border-b">
             <img
-              className="w-[50px] h-[50px] object-cover rounded-full border"
+              className="w-[50px] h-[50px] object-cover rounded-full"
               src={studyDetail.saveName ? studyDetail.saveName : "/logo.png"}
             />
             <div className="pl-3">
@@ -117,88 +119,85 @@ export default function StudyRecruitDetailPage(props) {
                 to={`/mypage/${userList.userId}`}
                 className="hover:text-[#989aff]"
               >
-                <div className="px-2.5 text-[15px] font-bold">
+                <div className="px-2.5 ext-[15px] font-bold">
                   {userList.nickname}
                 </div>
-                <div className="px-2.5 text-[13px]">{userList.email}</div>
               </NavLink>
             </div>
             <div className="absolute right-0">
               {/* 방장인 경우에는 스터디 수정창 아니면 스터디 신청  */}
-              {info.userId === userList.userId ? (
-                <Link to={"update"}>
-                  <button className="p-2.5 w-[100px] rounded-[10px] text-center text-[14px] border border-[#b1b2ff] hover:bg-[#b1b2ff]">
-                    수정하기
+              {info &&
+                (info.userId === userList.userId ? (
+                  <Link to={"update"}>
+                    <button className="text-[14px] text-center hover:font-bold">
+                      수정하기
+                    </button>
+                  </Link>
+                ) : (
+                  <button
+                    onClick={acceptHandler}
+                    className="text-[14px] text-center hover:font-bold"
+                  >
+                    스터디 신청
                   </button>
-                </Link>
-              ) : (
-                <button
-                  onClick={acceptHandler}
-                  className="p-2.5 w-[100px] rounded-[10px] text-center text-[14px] border border-[#b1b2ff] hover:bg-[#b1b2ff]"
-                >
-                  스터디 신청
-                </button>
-              )}
+                ))}
             </div>
           </div>
         </div>
 
         {/* 스터디 정보 부분  */}
-        <div className="w-9/12">
-          <div className="flex justify-around items-center border-b py-5">
+        <div className="w-9/12 py-7 bg-white text-2xl font-bold mt-10">
+          <div className="flex justify-around items-center">
             <div className="flex flex-col justify-center items-center">
-              <FontAwesomeIcon icon={faCalendar} className="mb-3 text-[20px]" />
-              <p className="text-sm">시작 예정일</p>
-              <p className="text-sm font-bold">{studyDetail.startTime}</p>
+              <FontAwesomeIcon icon={faCalendarDays} />
+              <p className="text-sm">시작예정일</p>
+              <p className="text-sm">{studyDetail.startTime}</p>
             </div>
             <div className="flex flex-col justify-center items-center">
-              <FontAwesomeIcon icon={faUser} className="mb-3 text-[20px]" />
-              <p className="text-sm">모집 인원</p>
-              <p className="text-sm font-bold">{studyDetail.userLimit} 명</p>
+              <FontAwesomeIcon icon={faUsers} />
+              <p className="text-sm">모집인원</p>
+              <p className="text-sm">{studyDetail.userLimit}</p>
             </div>
             <div className="flex flex-col justify-center items-center">
-              <FontAwesomeIcon
-                icon={faCircleCheck}
-                className="mb-3 text-[20px]"
-              />
-              <p className="text-sm">모집 방법</p>
+              <FontAwesomeIcon icon={faCircleCheck} />
+              <p className="text-sm">모집방법</p>
               {studyDetail.public ? (
-                <p className="text-sm font-bold"> 공개 </p>
+                <p className="text-sm"> 공개 </p>
               ) : (
-                <p className="text-sm font-bold"> 신청</p>
+                <p className="text-sm"> 수락</p>
               )}
             </div>
           </div>
-          <div className="container py-7 border-b">
-            <p className="text-xl font-bold pb-3">상세 설명</p>
-            <div className="text-[15px] break-all">{studyDetail.content}</div>
+          <div className="container mt-10">
+            <div>{studyDetail.content}</div>
           </div>
-          <div className="py-7">
-            <p className="text-xl font-bold pb-3">커리큘럼</p>
-            {curriculum.length > 2 ? (
-              <Slider {...settings}>
-                {curriculum.map((item) => {
-                  return (
-                    <MainCourse
-                      key={item.course.course_id}
-                      propData={item.course}
-                    />
-                  );
-                })}
-              </Slider>
-            ) : (
-              <div className="flex justify-around items-center w-full gap-2.5">
-                {curriculum.map((item) => {
-                  return (
-                    <MainCourse
-                      key={item.course.course_id}
-                      propData={item.course}
-                    />
-                  );
-                })}
-              </div>
-            )}
-          </div>
+
+          <p className="flex-grow-0 flex-shrink-0 text-xl font-bold text-left mt-10">
+            커리큘럼
+          </p>
+          {curriculum.length > 2 ? (
+            <Slider {...settings}>
+              {curriculum.map((item) => {
+                return (
+                  <MainCourse
+                    key={item.course.course_id}
+                    propData={item.course}
+                  />
+                );
+              })}
+            </Slider>
+          ) : (
+            <div className="flex justify-around items-center flex-grow-0 flex-shrink-0 w-full gap-2.5 pl-2.5 pb-2.5">
+              {curriculum.map((item) => {
+                return (
+                  <MainCourse
+                    key={item.course.course_id}
+                    propData={item.course}
+                  />
+                );
+              })}
+            </div>
+          )}
         </div>
 
         {showOpenModal ? (
@@ -210,6 +209,12 @@ export default function StudyRecruitDetailPage(props) {
         {showNotOpenModal ? (
           <StudyRecruitModalNotOpen onCancel={closeModalHandler} />
         ) : null}
+        {showModal ? (
+        <LoginModal
+          onCancel={closeLoginHandler}
+          onConfirm={closeLoginHandler}
+        />
+      ) : null}
       </div>
     </>
   );
