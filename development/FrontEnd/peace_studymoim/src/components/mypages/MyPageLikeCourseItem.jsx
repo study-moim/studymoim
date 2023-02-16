6cebfb70688ff835243e13c6352ae166868fb1b5
cjfal2 <cjfal2@kakao.com>
2023-02-16 09:15:12 +0900

    Fix: 옛날 디자인이 살아있어서 죽임

--- development/FrontEnd/peace_studymoim/src/components/mypages/MyPageLikeCourseItem.jsx
+++ development/FrontEnd/peace_studymoim/src/components/mypages/MyPageLikeCourseItem.jsx
@@ -3,49 +3,50 @@ import { Link } from "react-router-dom";
 
 export default function MyPageLikeCourseItem({ propData }) {
   const [isHover, setIsHover] = useState(false);
-  const slicedTitle = propData.title.substring(0, 25) + "...";
+  const slicedTitle = propData.title.substring(0, 30) + "...";
+  const slicedContent = propData.content.substring(0, 100) + "...";
 
   return (
     <div
-      className="relative w-[234px]"
+      className="relative w-[180px]"
       onMouseEnter={() => setIsHover(true)}
       onMouseLeave={() => setIsHover(false)}
     >
-      <div className="flex flex-col justify-start items-center h-[245px] relative gap-4 bg-white shadow-lg rounded-md">
+      <div className="h-[190px] relative mb-1">
         <img
           src={
             propData.thumbnail !== "path/to/image"
               ? propData.thumbnail
               : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRebJh2gLipfb2bfmUkZSf7U39YW-7jxMH5_A&usqp=CAU"
           }
-          className="w-full h-[146px] object-cover"
+          className="w-full h-[100px] object-cover rounded-[15px]"
         />
-        <p className="w-[209px] h-[65px] text-xl font-bold text-left text-black">
-          {propData.title.length > 24 ? slicedTitle : propData.title}
-        </p>
+        <div className="w-full">
+          <p className="text-[13px] px-3 pt-3 h-[60px]">
+            {propData.title.length > 30 ? slicedTitle : propData.title}
+          </p>
+          <p className="text-[12px] px-3">
+            {propData.courseProviderName}
+          </p>
+        </div>
       </div>
       {isHover ? (
         <Link
-          to={`/course/${propData.course_id}`}
-          state={{
-            propData: propData,
-          }}
+              to={`/course/${propData.course_id}`}
+              state={{
+                propData: propData
+              }}
         >
-          <div className="w-full h-[245px] absolute top-0 bg-neutral-800 opacity-90">
-            <div className="h-full flex flex-col justify-between items-center pt-2">
-              <p className="w-[209px] h-[60px] text-base font-bold text-left text-white cursor-pointer hover:scale-105 hover:text-[#9a71fa]">
-                {propData.title}
-              </p>
-              <div className="flex justify-center items-center mb-5">
-                <p className="w-full h-fit text-base font-bold text-left mt-3 text-white">
-                  채널명: {propData.courseProviderName}
-                </p>
-                {/* <img src="/figma.png" alt="x" className="w-[70px] h-[70px] mt-3 rounded-full cursor-pointer hover:scale-105" /> */}
-                {/* <p className="left-0 bottom-0 text-4xl text-red-700 cursor-pointer hover:scale-105 hover:skew-y-12">
-                    ♥
-                  </p> */}
-              </div>
-            </div>
+          <div className="w-full h-[190px] absolute top-0 bg-neutral-900 opacity-90 rounded-[15px] cursor-pointer p-5">
+            <p className="text-[15px] font-bold text-white">
+              {propData.title}
+            </p>
+            <p className="text-[13px] text-white mt-3">
+              {propData.content.length > 100 ? slicedContent : propData.content}
+            </p>
+            <p className="text-[13px] text-white mt-5">
+                채널명: {propData.courseProviderName}
+            </p> 
           </div>
         </Link>
       ) : null}
